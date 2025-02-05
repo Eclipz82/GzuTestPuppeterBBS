dd2150c51e64f6dcb7de27483d74a7c47ef2c8b8
Eclipz82 <germans.zukovs@lspa.lv>
2025-02-05 18:47:13 +0200

    Initial commit

---
+++ src/test.ts
@@ -0,0 +1,87 @@
+import puppeteer from "puppeteer";
+
+(async function createRikojumi() {
+    const username = "0720SP_admin";
+    const password = "8056bKyRo";
+    const url = `http://${username}:${password}@bbsproduct05.bbsolutions.lv`;
+    
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(url);
+    
+    const urlRikojumi = "https://bbsproduct05.bbsolutions.lv/sites/lietvediba/Lists/Rikojumi/AllItems.aspx";
+    await page.goto(urlRikojumi);
+    
+    // Селекторы
+    const buttonJaunsVienums = "#idHomePageNewItem";
+    const fieldUznemums = '#wrapper > div.container-fluid > div:nth-child(1) > div.col-sm-12.col-md-12.col-lg-6 > div > div:nth-child(1) > div:nth-child(1) > div > div.field-card__inner.svelte-fmmd3u > div > span > div > div.sv-control.svelte-1kcptsp > div.sv-content.sv-input-row.svelte-1kcptsp';
+    const holdingHeadquaterSelector = '[title="Holding Headquater"]';
+    const fieldLietasIndekss = '#wrapper > div.container-fluid > div:nth-child(1) > div.col-sm-12.col-md-12.col-lg-6 > div > div:nth-child(1) > div:nth-child(2) > div > div.field-card__inner.svelte-fmmd3u > div > span:nth-child(1) > span > div > div > div.sv-content.sv-input-row.svelte-1kcptsp';
+    const lietasIndekssSelector = '[title="1.1.1 IJU Test (2024)"]';
+    const fieldDokumentaPiejamiba = '#wrapper > div.container-fluid > div:nth-child(1) > div.col-sm-12.col-md-12.col-lg-6 > div > div:nth-child(1) > div:nth-child(4) > div > div.field-card__inner.svelte-fmmd3u > select';
+    const fieldPar = '#wrapper > div.container-fluid > div:nth-child(1) > div.col-sm-12.col-md-12.col-lg-6 > div > div:nth-child(2) > div:nth-child(1) > div > div.field-card__inner.svelte-fmmd3u > input[type=text]';
+    const buttonGenerateDokumentu = '#stickRibbon > div.left-nav > div > button';
+    const fieldParakstisanasVeids = '#wrapper > div.container-fluid > div:nth-child(1) > div:nth-child(2) > div > div > div:nth-child(1) > div > div.field-card__inner.svelte-fmmd3u > select';
+    const fieldIzstradatajs = '#BBS_WF_ResponsiblePerson_8898caa6-cdd6-4cb8-8f28-b8fcc81f11f6_\\$ClientPeoplePicker';
+    const executionPickerSelector = '#BBS_ResponsibleForExecution_7f86d46d-1cdc-497e-a4fa-7facda65ad9e_\\$ClientPeoplePicker';
+    const checkboxSelector = '#ft-id-1 > li:nth-child(1) > span > span.fancytree-checkbox > input';
+    const startButtonSelector = '#stickRibbon > div.right-nav > div:nth-child(1) > button';
+    
+    // Переход по селекторам и действия
+    await page.waitForSelector(buttonJaunsVienums);
+    await page.click(buttonJaunsVienums);
+
+    await page.waitForSelector(fieldUznemums);
+    await page.click(fieldUznemums);
+
+    await page.waitForSelector(holdingHeadquaterSelector);
+    await page.click(holdingHeadquaterSelector);
+
+    await page.waitForSelector(fieldLietasIndekss);
+    await page.click(fieldLietasIndekss);
+
+    await page.waitForSelector(lietasIndekssSelector);
+    await page.click(lietasIndekssSelector);
+
+    await page.waitForSelector(fieldDokumentaPiejamiba);
+    await page.click(fieldDokumentaPiejamiba);
+    await page.keyboard.press("ArrowDown");
+    await page.keyboard.press("Enter");
+
+    await page.waitForSelector(fieldPar);
+    await page.type(fieldPar, "Test");
+
+    await page.click(buttonGenerateDokumentu);
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    await page.waitForSelector(fieldParakstisanasVeids);
+    await page.click(fieldParakstisanasVeids);
+    await page.keyboard.press("ArrowDown");
+    await page.keyboard.press("Enter");
+
+    await page.waitForSelector(fieldIzstradatajs);
+    await page.click(fieldIzstradatajs);
+    await page.keyboard.press('Backspace');
+    await page.type(fieldIzstradatajs, 'Regular Clerk 1');
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Enter");
+
+    await page.click(executionPickerSelector);
+    await page.type(executionPickerSelector, 'Regular Clerk 1');
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Enter");
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    await page.waitForSelector(checkboxSelector);
+    await page.click(checkboxSelector);
+
+    // Нажимаем кнопку старт
+    await page.waitForSelector(startButtonSelector);
+    await page.click(startButtonSelector);
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    // await browser.close();
+})();
